b3cbd69d7360ca58b719436a2cacfb29abcf2d4e
MelvinSam2000 <melvinsamxd@gmail.com>
2019-03-08 08:46:17 -0500

    Finish GameModel, not tested

--- 01/GameModel.java
+++ 01/GameModel.java
@@ -1,5 +1,60 @@
 public class GameModel {
+	
+	private int width;
+	private int height;
+	private boolean board[][]; 
 
- // Your code here
+	public GameModel(int width, int height) {
+		
+        this.width = width;
+		this.height = height;
+		board = new boolean[height][width];
+	}
 
+	public int getHeight() {
+		
+        return height;
+	}
+
+	public int getWidth() {
+		
+        return width;
+	}
+
+    //Column i, Row j
+	public boolean isON(int i, int j) {  
+        
+        return board[i][j];
+	}
+
+    public void reset() {
+        
+        board = new boolean[height][width];
+    }
+
+    //Column j, Row i
+    public void set(int i, int j, boolean value) {
+        
+        board[j][i] = value;
+    }
+
+    public String toString() {
+
+        String out = "[";
+
+        for (int i = 0; i < height; i++) {
+
+            out += "[";
+
+            for (int j = 0; j < width; j++) {
+                out += board[i][j];
+                out += (j == width - 1) ? "" : ", ";
+            }
+
+            out += (i == height - 1) ? "]]\n" : "],\n";
+
+        }
+
+        return out;
+    }
 }
